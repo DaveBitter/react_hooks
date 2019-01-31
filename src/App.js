9e335ecc33eb41e1328dba01fd8011dd31361bbb
Dave Bitter <dbitter@mirabeau.nl>
2019-01-31 16:50:03 +0100

    feat(counter-with-use-context): Added example

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import AppProvider from "./static/AppProvider";
 // Components
 import Counter from "./components/Counter/Counter.js";
 import CounterWithUseReducer from "./components/Counter/CounterWithUseReducer.js";
+import CounterWithUseContext from "./components/Counter/CounterWithUseContext.js";
 
 // Component
 class App extends Component {
@@ -24,6 +25,13 @@ class App extends Component {
           <h1>useReducer hook</h1>
           <section>
             <CounterWithUseReducer />
+          </section>
+        </article>
+
+        <article>
+          <h1>useContext hook</h1>
+          <section>
+            <CounterWithUseContext />
           </section>
         </article>
 
--- src/static/AppProvider.js
+++ src/static/AppProvider.js
@@ -4,7 +4,14 @@ import React, { Component } from "react";
 import { AppContext } from "./Context";
 
 class AppProvider extends Component {
-  state = {};
+  state = {
+    count: 0,
+    setCount: this._setCount.bind(this)
+  };
+
+  _setCount(count) {
+    this.setState({ count });
+  }
 
   render() {
     return (
